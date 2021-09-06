a021247ecd460d044876337678d040bf161645d0
Aakash_03 <57516901+Aakash-0003@users.noreply.github.com>
2021-09-06 12:25:41 +0530

    Create 1.print-n-numbers.js

---
+++ 1.print-n-numbers.js
@@ -0,0 +1,18 @@
+/* Simple Hello World in Node.js */
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+rl.question("enter the number", function(answer) {
+    const input = answer;
+    print(input);
+});
+
+function print(i) {
+    if (i > 0) {
+        print(i - 1);
+        console.log(i + " ");
+    }
+    return;
+}
